439ce6bd750f61b3ef73304b79c0380ac823fc4e
Mina Nazari <mina.nazary66@gmail.com>
2024-12-12 15:06:29 +0330

    Some changes were made for responsive achieves.

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,9 +5,9 @@ import { CiSearch } from "react-icons/ci";
 
 function Menu() {
   return (
-    <nav className="navbar navbar-expand-md navbar-dark">
+    <nav className="navbar navbar-expand-lg navbar-dark">
       <div className="container h-100">
-        <Link to="/" className="navbar-brand d-md-none" ><img src={logo} alt="Industrial Logo" width="200px" /></Link>
+        <Link to="/" className="navbar-brand d-lg-none" ><img src={logo} alt="Industrial Logo" width="200px" /></Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
--- src/components/TopHeader.jsx
+++ src/components/TopHeader.jsx
@@ -10,19 +10,19 @@ const TopHeader = () => {
       <div className="top-header">
       <div className="container">
         <div className="row justify-content-between align-items-center">
-          <div className='col-6'><p>Welcome to Leader in Industrial Solution since 2005</p></div>
-          <div className='col-6'><p className='text-end'>Global Certificate: <span>ISO 9001:2015 </span></p></div>
+          <div className='col-12 col-md-6 text-center text-md-start'><p>Welcome to Leader in Industrial Solution since 2005</p></div>
+          <div className='col-12 col-md-6 text-center text-md-end'><p>Global Certificate: <span>ISO 9001:2015 </span></p></div>
         </div>
       </div>
       </div>
       <div className="container-fluid-sm container-md">
         <div className="row justify-content-between align-items-center py-4">
-          <div className="logo col-md-4 col-lg-6 d-none d-md-block">
+          <div className="logo col-lg-6 d-none d-lg-block">
             <Link to="/">
               <img src={logo} alt="Industrial Logo" width="200px" />
             </Link>
           </div>
-          <div className="contact-info col-12 col-md-8 col-lg-6 ">
+          <div className="contact-info col-12 col-lg-6 ">
             <div className="d-flex justify-content-around justify-content-lg-end align-items-center">
               <div className='d-flex justify-content-between align-items-center'>
                 <img src={location} alt="location" width="35px"/>
@@ -39,8 +39,8 @@ const TopHeader = () => {
                   <span className='fw-bold'>18004567890</span>
                 </div>
               </div>
-              <div className='ms-4'>
-                <button className='btn btn-warning'>REQUEST A QUOTE</button>
+              <div className='ms-4 d-none d-md-block'>
+                <button className='btn btn-warning fs-6 fs-md-4'>REQUEST A QUOTE</button>
               </div>
             </div>
           </div>
